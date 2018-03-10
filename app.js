032b9c149d960a021872befda7c058210ee19e2b
Ben <benjamin.a.spencer@gmail.com>
2018-03-09 23:46:34 -0500

    refactoring to just use draw a card api call, add chalk middleware

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const baseURL = 'https://deckofcardsapi.com/api/deck/'
+const chalk = require('chalk');
+const log = console.log;
 
 var request = require('request');
 var pokerEvaluator = require('poker-evaluator');
@@ -9,44 +11,43 @@ function findBestHand(cardHand) {
     return pokerEvaluator.evalHand(cardHand);
 }
 
+function drawCards() {
+    var numberOfCards = process.argv[2];
+    var deck_id = (process.argv[3] === undefined ? 'new' : process.argv[3]);
+    var url = baseURL + deck_id + '/draw/?count=' + numberOfCards.toString();
+    log(chalk.yellow('You chose a ' + numberOfCards + ' card hand.'));
+
+    parseRequest(url);
+}
+
 //TODO name this function better
-function requestAtrribute(url, attr) {
+function parseRequest(url) {
     request(url, function (error, response, body) {
         var parsedBody;
         var handofCards = [];
+
         if (!error && response.statusCode === 200) {
 
-            parsedBody = JSON.parse(body)[attr];
-            if(attr === 'cards') {
-                parsedBody.forEach(function(card) {
-                    console.log(card['value'] + ' of ' + card['suit']);
-                    handofCards.push(card['code']);
+            parsedBody = JSON.parse(body);
+            log(chalk.green('===== YOUR POKER HAND ====='));
 
-                });
-                console.log('Your best hand is: ' + findBestHand(handofCards)['handName']);
-            } else{
-                console.log('deck body: ' + body);
-            }
+            parsedBody['cards'].forEach(function(card) {
+                if(card['value'] === '10') {
+                    //Fixes API and poker-eval incompatability
+                    card['code'] = card['code'].replace(0, 'T')
+                }
+                handofCards.push(card['code']);
+                log(card['value'] + ' of ' + card['suit']);
+
+            });
+            log('Your best hand is: ' + chalk.red(findBestHand(handofCards)['handName'].toUpperCase()));
+            log(chalk.blue('There are ' + parsedBody['remaining']
+                + ' cards remaining in deck ' + parsedBody['deck_id']));
+
         } else {
-            console.log('error:' +response.statusCode);
-            console.log(body);
+            log(chalk.red('Error:') +response.statusCode);
         }
     });
 }
 
-//assume user wants only 1 deck for now
-function getSingleDeck() {
-    var url = baseURL+'new/shuffle/?deck_count=1';
-    var attr = 'deck_id'
-    requestAtrribute(url, attr);
-}
-
-// if deck_id = new: creates a shuffled deck and draws cards from that deck in the same request.
-function drawCards(deck_id, numberOfCards) {
-    var url = baseURL+ deck_id + '/draw/?count=' + numberOfCards;
-    var attr = 'cards';
-    requestAtrribute(url, attr);
-}
-
-//assume user always draws five cards from a new single deck
-drawCards('new', 5);
+drawCards();
